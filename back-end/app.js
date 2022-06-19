101ac6bfcda556dc0ee2c85a16f461a01f83362a
AlineCantalice <alinedecantalice@gmail.com>
2022-06-19 15:43:19 -0300

    BONUS - paginação e ajustes

--- back-end/app.js
+++ back-end/app.js
@@ -10,7 +10,7 @@ const tweets = [];
 
 app.post('/sign-up', (req, res) => {
     const { username, avatar } = req.body;
-    if (!isEmpty(username) && isValidUrl(avatar)) {
+    if (!isEmpty(username) && isValidType(username) && isValidUrl(avatar)) {
         users.push(req.body);
         res.status(201).send('OK');
     } else {
@@ -22,7 +22,7 @@ app.post('/tweets', (req, res) => {
     const user = req.headers.user;
     const { tweet } = req.body;
     const { avatar } = users.find(u => u.username === user);
-    if (!isEmpty(user) && !isEmpty(tweet) && isValidUrl(avatar)) {
+    if (!isEmpty(user) && isValidType(user) && !isEmpty(tweet) && isValidType(tweet) && isValidUrl(avatar)) {
         tweets.push({
             username: user,
             avatar: avatar,
@@ -39,17 +39,21 @@ app.get('/tweets', (req, res) => {
         res.sendStatus(400);
     } else {
         const page = req.query.page;
-        if (tweets.length < 10) {
-            res.send([...tweets].reverse());
+        if (tweets.length > 0 && page > Math.ceil(tweets.length / 10)) {
+            res.status(400).send('Informe uma página válida!');
         } else {
-            res.send([...tweets].reverse().slice(0, 10));
+            const pageTweets = [];
+            for (let i = tweets.length - (page - 1) * 10 - 1; (i > -1 && i >= tweets.length - page * 10); i--) {
+                pageTweets.push(tweets[i]);
+            }
+            res.send(pageTweets);
         }
     }
 });
 
 app.get('/tweets/:username', (req, res) => {
     const user = req.params.username;
-    if (isEmpty(user)) {
+    if (isEmpty(user) || !isValidType(user) ) {
         res.sendStatus(400);
     } else {
         const userTweets = tweets.filter(u => u.username === user);
@@ -67,11 +71,18 @@ function isEmpty(obj) {
 
 function isValidUrl(url) {
     const URL_REGEX = /^https?:\/\/.*\/.*\.(png|gif|webp|jpeg|jpg)\??.*$/gmi;
-    if (!isEmpty(url)) {
+    if (!isEmpty(url) && isValidType(url) ) {
         if (URL_REGEX.test(url)) {
             return true;
         }
         return false;
+    }
+    return false;
+}
+
+function isValidType(obj) {
+    if (typeof obj === 'string') {
+        return true;
     }
     return false;
 }
